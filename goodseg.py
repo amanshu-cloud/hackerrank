de06a64c1778aa54f91ce45afb22fb6781c54ffe
Amanshu <55711996+amanshu-cloud@users.noreply.github.com>
2021-07-24 15:43:24 +0530

    adding pairwise function

--- goodseg.py
+++ goodseg.py
@@ -1,6 +1,12 @@
 from more_itertools import pairwise
 
-def goodseg(badnumbers,lower,upper):
+
+
+#define pairwise function if not available
+def pairwise(numbers):
+    return zip(numbers[:-1],numbers[1:])
+    
+def goodsegment(badnumbers,lower,upper):
     badnumbers = [lower-1] + sorted(x for x in badnumbers if lower <=x <= upper)
     difference = (b-a for a,b in pairwise(badnumbers))
 
